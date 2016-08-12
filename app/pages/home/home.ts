4ea3275aee0e5ff361ed0d765e54860ecd71cb62
Alex <alexandre.piel@gmail.com>
2016-08-12 22:56:04 +0200

    refresh status

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
+import { Http, Response } from '@angular/http';
+
 import { ItemBundleService } from '../../itemBundle.service';
 import { ItemBundle } from '../../itemBundle';
+import { Item } from '../../item';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -10,9 +13,10 @@ export class Home implements OnInit {
     itemsBundles: ItemBundle[];
     errorMessage: string;
 
-    constructor(private itemBundleService: ItemBundleService) { }
+    constructor(private itemBundleService: ItemBundleService, private http: Http) { }
 
     getItemsBundles() {
+      console.log('Load items bundles');
       this.itemBundleService.getItemsBundles()
                       .subscribe(
                          //itemsBundles => this.itemsBundles = itemsBundles,
@@ -23,17 +27,31 @@ export class Home implements OnInit {
     parseItemsBundles(home: Home, itemsBundles: ItemBundle[]) {
         for (let itemBundle of itemsBundles) {
             for (let item of itemBundle.items) {
-                if (item.statusUrl) {
-                    item.statusObserver.subscribe(
-                                            status => item.status = status,
-                                            error =>  this.errorMessage = <any>error);
-                }
+                home.getItemStatus(home, item);
             }
         }
         home.itemsBundles = itemsBundles;
     }
+    
+    getItemStatus(home: Home, item: Item) {
+        if (item.statusUrl) {
+            item.statusObserver.subscribe(
+                                    status => item.status = status,
+                                    error =>  home.errorMessage = <any>error);
+        }        
+    }
         
     ngOnInit() {
+      var me = this;
       this.getItemsBundles();
-    }    
+      //setInterval(function(){ me.getItemsBundles(); }, 5000);
+    }   
+    
+    clickAction(item: Item) {
+        var me = this;
+        this.http.get(item.actionUrl).map(function(res) { 
+            //console.log(res);
+            me.getItemStatus(me, item)
+        }).subscribe();
+    } 
 }
